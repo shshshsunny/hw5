4c662a10fad47d79adca542b7d37ebacd6b92e0a
shshshsunny <sunny940911@gmail.com>
2026-06-08 21:59:02 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/hw5/',
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
