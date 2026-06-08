0b0ca6c749614da810b243b8defe68bd00f5e5f4
shshshsunny <sunny940911@gmail.com>
2026-06-08 22:07:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/hw5/',
   plugins: [vue()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: 'dist',          // 強制輸出到根目錄的 dist 資料夾
+    emptyOutDir: true,       // 打包前先清空舊檔案
+    assetsDir: 'assets',     // 確保 JS/CSS 放在 assets 子目錄內
   }
 })
